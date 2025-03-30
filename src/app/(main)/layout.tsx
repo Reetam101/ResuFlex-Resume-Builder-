0988896096b0b2da1306c9c0f64375ab7dc81d9b
Reetam101 <reetamraj2@gmail.com>
2025-03-30 20:41:00 +0530

    update: updated the UI for the landing page and made it functional according to the currently logged in user

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
+import PremiumModal from "@/components/premium/PremiumModal";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div className="flex min-h-screen flex-col">
     <Navbar />
     {children}
+    <PremiumModal />
   </div>
 }
---
+++ src/components/premium/PremiumModal.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+
+export default function PremiumModal() {
+  return (
+    <Dialog open>
+      <DialogContent className="max-w-2xl">
+        <DialogHeader>
+          <DialogTitle>
+            ResuFlex Premium
+          </DialogTitle>
+        </DialogHeader>
+        <div className="space-y-6">
+          <p>Get a premium subscription to unlock more features</p>
+          <div className="flex">
+            <div className="flex w-1/2 flex-col space-y-5">
+              <h3 className="text-center text-lg font-bold">Premium</h3>
+            </div>
+            <div className="border-l mx-6" />
+            <div className="flex w-1/2 flex-col space-y-5">
+              <h3 className="text-center text-lg font-bold">Premium plus</h3>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
